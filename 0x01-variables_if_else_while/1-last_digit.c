d3f833d698a3d2196508a13906246e9f92de4009
DeeteeAnn <dolliee.annobil@yahoo.com>
2023-06-16 12:13:24 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ int main(void)
 	{
 		printf("last figure of %d is %d and is 0\n", n, lf);
 	}
-	else (lf < 6 && lf !=0)
+	else
 	{
 		printf("last figure of %d is %d and is less than 6 and not 0\n", n, lf);
 	}
