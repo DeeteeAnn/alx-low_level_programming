dc1d2e2f9736e2f66b086febcbc74c2203e017a7
DeeteeAnn <dolliee.annobil@yahoo.com>
2023-06-16 15:05:14 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description: 'list all single digit numbers from base ten'
+ *Return: Always 0
+ */
+int main(void)
+{
+	int m;
+
+	for (m=0; m <=9; m++)
+	{
+		printf("%d", m);
+	}
+	printf("\n");
+        return (0);
+}
