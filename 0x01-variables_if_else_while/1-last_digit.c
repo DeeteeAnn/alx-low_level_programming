3bdb999e461755de3cdda421c04eee667be3f777
DeeteeAnn <dolliee.annobil@yahoo.com>
2023-06-16 11:04:41 -0700

    last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Obtain the last figure of a number'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int lf;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lf = n % 10;
+	if (lf > 5)
+	{
+		printf("last figure of %d is %d and is greater than 5", n, lf);
+	}
+	else if (lf == 0)
+	{
+		printf("last figure of %d is %d and is 0", n, lf);
+	}
+	else
+	{
+		printf("last figure of %d is %d and is less than 6 and not 0", n, lf);
+	}
+	return (0);
+}
