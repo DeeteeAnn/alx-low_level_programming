e419a166d31fdf90dc34cbea73a856312f4b2d77
DeeteeAnn <dolliee.annobil@yahoo.com>
2023-06-16 17:24:07 -0700

    print combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Details: 'Print all combination os two digits'
+ *Return: Always 0
+ */
+int main(void)
+{
+	int num1 = 0, num2;
+
+	while (num1 <= 9)
+	{
+		num2 = 0;
+		while (num2 <= 9)
+		{
+			if (num1 != num2 && num1 < num2)
+			{
+				putchar(num1 + 48);
+				putchar(num2 + 48);
+
+				if (num1 + num2 != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			num2++;
+		}num1++;
+	}
+	putchar('\n');
+	return (0);
+}
