76fda4078fe3c0a390a1be708fcb20e06757bf90
DeeteeAnn <dolliee.annobil@yahoo.com>
2023-10-02 06:59:18 -0700

    This function creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,7 +1,20 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+
+/**
+ * _strlen- length of string is returned
+ * @s: length of string to check
+ * Return: figure of the string length
+ */
+int _strlen(char *s)
+{
+	int ab = 0;
+
+	if (s == 0)
+		return (0);
+	while (*s++)
+		ab++;
+	return (ab);
+}
 
 /**
  * create_file - a new file is created
@@ -11,21 +24,18 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	FILE *doc;
-	size_t len;
+	ssize_t bytes = 0, len = _strlen(text_content);
+	int doc;
 
 	if (filename == NULL)
 		return (-1);
 
-	doc = fopen(filename, "w");
-	if (doc == NULL)
+	doc = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (doc == -1)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		len = strlen(text_content);
-		fwrite(text_content, sizeof(char), len, doc);
-	}
-	fclose(doc);
-	return (1);
+	if (len)
+		bytes = write(doc, text_content, len);
+	close(doc);
+	return (bytes == len ? 1 : -1);
 }
